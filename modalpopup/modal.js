000f7b93cab93aa73ad61893b7076e6fbe9fa22b
emajidev <emajidev@gmail.com>
2019-03-05 01:53:07 -0500

    access sing in pages

--- modalpopup/modal.js
+++ modalpopup/modal.js
@@ -1,25 +1,35 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+// Get the modalin
+var modalin = document.getElementById('Modal-singin');
+var modalup = document.getElementById('Modal-singup');
+// Get the button that opens the modalin
+var btnin = document.getElementById("btn-singin");
+var btnup = document.getElementById("btn-singup");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// Get the <span> element that closes the modalin
+var spanin = document.getElementsByClassName("closein")[0];
+var spanup = document.getElementsByClassName("closeup")[0];
+// When the user clicks on the button, open the modalin 
+btnin.onclick = function() {
+  modalin.style.display = "block";
+}
+btnup.onclick = function() {
+  modalup.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// When the user clicks on <span> (x), close the modalin
+spanin.onclick = function() {
+  modalin.style.display = "none";
+}
+spanup.onclick = function() {
+  modalup.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modalin, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalin) {
+    modalin.style.display = "none";
+  }
+  if (event.target == modalup) {
+    modalup.style.display = "none";
   }
 }
